d4c6d061c015be199d290412934bcceb50b5b50a
Cara Eppes <ceppes@udel.edu>
2019-03-31 19:37:36 -0400

    completed fibb1 and sundays

--- fibb1.py
+++ fibb1.py
@@ -1,1 +1,34 @@
+# Sum of Even Terms of Fibonacci Sequence < 4,000,000
 
+
+def is_even(num):
+    return num % 2 == 0
+
+
+def get_even_terms():
+    term1 = 0
+    term2 = 1
+    evens = []
+    while term2 <= 4000000:
+        sumOfTerms = term1 + term2
+        if is_even(term2):
+            evens.append(term2)
+        term1 = term2
+        term2 = sumOfTerms
+    return evens
+
+
+def sum_of_even_terms():
+    sumOfEvens = 0
+    evens = get_even_terms()
+    for even in evens:
+        sumOfEvens += even
+    return sumOfEvens
+
+
+# print(is_even(4))  # true
+# print(is_even(3))  # false
+# print(get_even_terms()) # [2, 8, 34, 144, 610, 2584, 10946, 46368, 196418, 832040, 3524578]
+
+print("The sum of the even terms of the Fibonacci sequence whose values are less than 4000000 is %d."
+     % sum_of_even_terms())
--- sundays.py
+++ sundays.py
@@ -1,1 +1,13 @@
+from datetime import date
 
+
+def number_of_sundays_on_first_of_month():
+    sundays = 0
+    for year in range(1901, 2001):
+        for month in range(1, 13):
+            if date(year, month, 1).weekday() == 6:
+                sundays += 1
+    return sundays
+
+
+print("There were %d Sundays on the first of the month in the 20th century." % number_of_sundays_on_first_of_month())
