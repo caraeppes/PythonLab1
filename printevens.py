36dc02db3fb64f91fefed6cbbc108769bda851e7
Cara Eppes <ceppes@udel.edu>
2019-04-01 01:41:21 -0400

    completed parts 1 through 7

--- printevens.py
+++ printevens.py
@@ -1,1 +1,15 @@
+# Print even numbers that occur before 237 in sequence
 
+
+def print_evens_before_237(inputList):
+    for int in inputList:
+        if int == 237:
+            break;
+        elif int % 2 == 0:
+            print(int)
+
+
+numbers = [386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345, 399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217, 815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717, 958,743, 527]
+
+print("The even numbers in the list that occur before 237 are:")
+print_evens_before_237(numbers)
--- wordfreqs.py
+++ wordfreqs.py
@@ -1,1 +1,26 @@
 
+def long_text_to_list():
+    string = open('longtext.txt').read()
+    return string.split()
+
+
+def word_frequency_dict(wordList):
+    wordDict = {}
+    for word in wordList:
+        if word in wordDict:
+            wordDict[word] = wordDict[word] + 1
+        else:
+            wordDict[word] = 1
+    return wordDict
+
+
+def print_250_most_common_words(wordDict):
+    sortedDict = sorted(wordDict.items(), key=lambda item: (item[1], item[0]))
+    top250 = 0
+    print("250 most common words and number of occurrences:")
+    while (top250 <= 250):
+        print(sortedDict.pop())
+        top250 += 1
+
+
+print_250_most_common_words(word_frequency_dict(long_text_to_list()))
